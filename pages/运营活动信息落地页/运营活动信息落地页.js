9366e5a9a3e93dd23cb29e6d579d91511cf50bac
p_sykang <kangsiyu@tencent.com>
2020-08-18 13:50:31 +0800

    1

--- pages/运营活动信息落地页/运营活动信息落地页.js
+++ pages/运营活动信息落地页/运营活动信息落地页.js
@@ -1,18 +1,53 @@
 // pages/运营活动信息落地页/运营活动信息落地页.js
+const app = getApp()
+var databaseUtil = require('../../utils/dataBaseUtil')
+const db = wx.cloud.database({
+  env: databaseUtil.getDataBaseEnv()
+})
+let interstitialAd = null
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    resultStr:'',
+    resultDes:'',
+    resultTips:'',
+    imageShow:false,
+    imageFile:'',
+    adOnceShow:false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    db.collection('configInfo').get({
+      success:function(res){
+        if (res.data.length) {
+           const element = res.data[0];
+           if (element.needActiveEntrance) {
+              that.setData({
+                resultStr:element.title,
+                resultDes:element.des,
+                resultTips:element.tips,
+                imageShow:element.imageFile.length>0,
+                imageFile:element.imageFile
+              })
+           }
+        }
+      }
+    })
+    if (wx.createInterstitialAd) {
+      interstitialAd = wx.createInterstitialAd({
+        adUnitId: 'adunit-5c619c8d7d63ae58'
+      })
+      interstitialAd.onLoad(() => {})
+      interstitialAd.onError((err) => {})
+      interstitialAd.onClose(() => {})
+    } 
   },
 
   /**
@@ -62,5 +97,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  calAction:function(){
+    if (interstitialAd && this.data.adOnceShow == false) {
+      var that = this;
+      that.setData({
+        adOnceShow:true
+       })
+      interstitialAd.show().catch((err) => {
+       console.error(err)
+       that.setData({
+        adOnceShow:false
+       })
+       
+    })
+    }
+    wx.previewImage({
+      urls: [this.data.imageFile],
+    })
   }
 })
